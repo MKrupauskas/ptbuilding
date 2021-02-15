250bd9b397cf3ec971f2282e60941ed36460f11e
Mykolas Krupauskas <mkrupauskas@gmail.com>
2021-02-15 12:22:46 +0200

    add google font cms registration

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,21 +11,19 @@ export default class Body extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <Helmet>
-          {!this.props.isPreview && (
+        {!this.props.isPreview && (
+          <Helmet>
             <title>
               {_.get(this.props, 'pageContext.frontmatter.title', null) &&
                 _.get(this.props, 'pageContext.frontmatter.title', null) +
                   ' - '}
               {_.get(this.props, 'pageContext.site.siteMetadata.title', null)}
             </title>
-          )}
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initialScale=1.0"
-          />
-          {!this.props.isPreview && (
+            <meta charSet="utf-8" />
+            <meta
+              name="viewport"
+              content="width=device-width, initialScale=1.0"
+            />
             <meta
               name="description"
               content={_.get(
@@ -34,14 +32,12 @@ export default class Body extends React.Component {
                 null,
               )}
             />
-          )}
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-            rel="stylesheet"
-          />
-          {!this.props.isPreview &&
-            _.get(
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+              rel="stylesheet"
+            />
+            {_.get(
               this.props,
               'pageContext.site.siteMetadata.favicon',
               null,
@@ -57,18 +53,15 @@ export default class Body extends React.Component {
                 )}
               />
             )}
-          {!this.props.isPreview && (
             <link href="/styles/main.css" rel="stylesheet" />
-          )}
-          {!this.props.isPreview && (
             <body
               className={
                 'palette-' +
                 _.get(this.props, 'pageContext.site.siteMetadata.palette', null)
               }
             />
-          )}
-        </Helmet>
+          </Helmet>
+        )}
         <div id="page" className="site">
           <Header {...this.props} />
           <main id="content" className="site-content">
