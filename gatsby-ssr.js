9967bde7d7e34e372a57d8add77b15e4531f7da9
Mykolas Krupauskas <mkrupauskas@gmail.com>
2022-05-11 00:22:07 +0300

    add meta domain verification tag

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,23 +4,23 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-const React = require("react");
-const withPrefix = require("./src/utils/withPrefix").default;
+const React = require('react');
+const withPrefix = require('./src/utils/withPrefix').default;
 
-exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
+exports.onRenderBody = function ({ setHeadComponents, setPostBodyComponents }) {
+  setHeadComponents([
+    <meta
+      name="facebook-domain-verification"
+      content="3la6ahcjbqsnt7gltzh1e0mqkvi7u8"
+    />,
+  ]);
 
-    setHeadComponents([
-        
-    ]);
-
-    setPostBodyComponents([
-        <React.Fragment>
-            <script src={withPrefix('assets/js/plugins.js')}/>
-            <script src={withPrefix('assets/js/main.js')}/>
-            <script src={withPrefix('assets/js/page-load.js')}/>
-            <script src={withPrefix('assets/js/page-unload.js')}/>
-            
-        </React.Fragment>
-    ]);
-
+  setPostBodyComponents([
+    <React.Fragment>
+      <script src={withPrefix('assets/js/plugins.js')} />
+      <script src={withPrefix('assets/js/main.js')} />
+      <script src={withPrefix('assets/js/page-load.js')} />
+      <script src={withPrefix('assets/js/page-unload.js')} />
+    </React.Fragment>,
+  ]);
 };
